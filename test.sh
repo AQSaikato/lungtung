44d93b6a57464420f897fa30c01695532fd68afb
AQSaikato <89082783+AQSaikato@users.noreply.github.com>
2023-04-05 22:00:35 +0700

    Update and rename test.sh to readme

--- test.sh
+++
@@ -1,5 +0,0 @@
-rm /etc/ufw/user.rules
-rm /etc/ufw/user6.rules
-cd /etc/ufw/
-wget https://raw.githubusercontent.com/AQSaikato/abcdxyz-fw/main/user.rules
-wget https://raw.githubusercontent.com/AQSaikato/abcdxyz-fw/main/user6.rules
