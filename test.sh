20fcae3d0adc9f84a0e5cad05b6d9de943e9059c
AQSaikato <89082783+AQSaikato@users.noreply.github.com>
2023-04-05 21:30:31 +0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,5 @@
+rm /etc/ufw/user.rules
+rm /etc/ufw/user6.rules
+cd /etc/ufw/
+wget https://raw.githubusercontent.com/AQSaikato/abcdxyz-fw/main/user.rules
+wget https://raw.githubusercontent.com/AQSaikato/abcdxyz-fw/main/user6.rules
