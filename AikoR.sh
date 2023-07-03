16975580191b0fbec2921c398e7e0201e24fde8f
AQSaikato <89082783+AQSaikato@users.noreply.github.com>
2023-07-03 10:12:18 +0700

    change

--- AikoR.sh
+++ AikoR.sh
@@ -2,12 +2,12 @@
 
 # Kiểm tra xem tập lệnh này được chạy với tư cách người dùng root hay không
 if [[ $EUID -ne 0 ]]; then
-   printf "Chạy tập lệnh này cần quyền root!\n"
+   echo "Tập lệnh này phải được chạy với tư cách người dùng root!"
    exit 1
 fi
 
 # Cài đặt AikoR
-printf "Đang cài đặt AikoR...\n"
+echo "Đang cài đặt AikoR..."
 wget --no-check-certificate -O install.sh https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR/install.sh
 bash install.sh
 
@@ -15,17 +15,16 @@ bash install.sh
 cd /etc/AikoR
 
 # Xoá file trong thư mục AikoR
-printf "Đang xoá các file trong thư mục AikoR...\n"
+echo "Đang xoá các file trong thư mục AikoR..."
 sudo rm /etc/AikoR/aiko.yml
 
 # Đường dẫn đến thư mục AikoR
-printf "Đang tải xuống các file cần thiết...\n"
+echo "Đang tải xuống các file cần thiết..."
 wget -O /etc/AikoR/aiko.yml https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/aiko.yml
 wget -O /etc/AikoR/privkey.pem https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/privkey.pem
 wget -O /etc/AikoR/server.pem https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/server.pem
 
-printf "Đã hoàn thành cài đặt!\n"
-
+echo "Đã hoàn thành cài đặt!"
 
 # Nhập giá trị NodeID mới từ người dùng
 read -p "Nhập giá trị NodeID mới: " nodeID
