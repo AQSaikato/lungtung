a1884c284c151892c7614acbeaaf1dff0277c052
AQSaikato <89082783+AQSaikato@users.noreply.github.com>
2023-07-03 09:59:02 +0700

    change

--- AikoR.sh
+++ AikoR.sh
@@ -15,14 +15,14 @@ sudo rm /etc/AikoR/aiko.yml
 
 # Đường dẫn đến thư mục AikoR
 wget -O /etc/AikoR/aiko.yml https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/aiko.yml
-wget -O /etc/AikoR/aiko.yml https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/privkey.pem
-wget -O /etc/AikoR/aiko.yml https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/server.pem
+wget -O /etc/AikoR/privkey.pem https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/privkey.pem
+wget -O /etc/AikoR/server.pem https://raw.githubusercontent.com/AQSaikato/lungtung/AikoR-file/server.pem
 
 # Nhập giá trị NodeID mới từ người dùng
 read -p "Nhập giá trị NodeID mới: " nodeID
 
 # Thay đổi giá trị NodeID trong file aiko.yml
-sed -i "s/NodeID: .*/NodeID: $nodeID/" "etc/AikoR/aiko.yml"
+sed -i "s/NodeID: .*/NodeID: $nodeID/" "$aikoDirectory/aiko.yml"
 
 # Khởi động lại AikoR
 AikoR restart
